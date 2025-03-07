49b609aa9c61a58163cdc8d8e1ca858b63f3234a
Oscar Alexis Dávila Reyes <104913466+Daro25@users.noreply.github.com>
2025-03-07 10:48:59 -0600

    actualice de nuevo el index.php

--- ''/src/php/index.php
+++ ''/src/php/index.php
@@ -1,6 +1,10 @@
 <?php
 echo `
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>404 Not Found</title>
 </head>
 <body>
@@ -11,7 +15,7 @@ echo `
     }
     window.onload = showError;
 </script>
-
 </body>
+</html>
     `;
 ?>
