5a222c9c84f9c0627a9348534beb040d22effd54
Oscar Alexis Dávila Reyes <104913466+Daro25@users.noreply.github.com>
2025-03-07 11:06:41 -0600

    .

--- ''/src/php/index.php
+++ ''/src/php/index.php
@@ -1,15 +1,3 @@
 <?php
-echo `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>404 Not Found</title>
-</head>
-<body>
-    <h1>Error: No hay conexión a Internet. Verifica tu red e inténtalo de nuevo.</h1>
-</body>
-</html>
-    `;
+echo '<h1>Error: No hay conexión a Internet. Verifica tu red e inténtalo de nuevo.</h1>';
 ?>
