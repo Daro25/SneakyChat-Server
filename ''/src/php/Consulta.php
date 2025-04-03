0d9a29db63daca039c69ae04fca36ec02fa150d3
Oscar Alexis Dávila Reyes <104913466+Daro25@users.noreply.github.com>
2025-04-03 10:45:47 -0600

    de nuevo otra sugerencia de chatgpt

--- ''/src/php/Consulta.php
+++ ''/src/php/Consulta.php
@@ -1,7 +1,7 @@
 <?php 
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-
+header("Access-Control-Allow-Methods: GET, POST");
+header("Access-Control-Allow-Headers: Content-Type");
 class Tabla 
 {
 	public $ID, $sala_Id, $FechayHora, $Texto, $User_Id;
