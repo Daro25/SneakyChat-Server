5aeef18aee6264627b8be4b9b623599c6802d770
Oscar Alexis Dávila Reyes <104913466+Daro25@users.noreply.github.com>
2025-04-22 15:45:12 -0600

    Actualizacion
    los cambios no tenian nada que ver con el error, pero ya tengo mis sospechas

--- ''/src/php/Consulta_Sala.php
+++ ''/src/php/Consulta_Sala.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST");
 header("Access-Control-Allow-Headers: Content-Type");
-//header("Content-Type: application/json");
+header("Content-Type: application/json");
 
 
 class Tabla 
