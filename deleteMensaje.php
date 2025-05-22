14d12cffdba083a1a229e25306d644f3b93642eb
Oscar Alexis Dávila Reyes <104913466+Daro25@users.noreply.github.com>
2025-05-22 16:17:16 -0600

    Subi el codigo que me mando jhoan para eliminar mensajes

---
+++ deleteMensaje.php
@@ -0,0 +1,37 @@
+<?php 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST");
+header("Access-Control-Allow-Headers: Content-Type");
+
+$user = 'droa';
+$server = 'localhost';
+$database = 'mensajer_a';
+$password = 'droaPluving$1';
+$conex = mysqli_connect($server, $user, $password, $database);
+
+if($conex) {
+    
+    $mensajeId = filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT);
+
+    if ($mensajeId !== false) {
+        
+        $stmt = $conex->prepare("DELETE FROM mensaje WHERE id = ?");
+        $stmt->bind_param("i", $mensajeId);
+        $stmt->execute();
+
+        if ($stmt->affected_rows > 0) {
+            echo 'El mensaje fue eliminado exitosamente.';
+        } else {
+            echo 'No se encontró ningún mensaje.';
+        }
+
+        $stmt->close();
+    } else {
+        echo 'Parámetro inválido.';
+    }
+
+    mysqli_close($conex);
+} else {
+    echo 'La comunicación no fue posible con la base de datos.';
+}
+?>
--- src/php/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo '<h1>Error: No hay conexión a Internet. Verifica tu red e inténtalo de nuevo.</h1>';
-?>
